48b3e341f72f48fe968ed6222d0f74f96ee6128f
maheenur13 <jahidun.nur@apsissolutions.com>
2023-10-20 00:25:41 +0600

    build issue solve

--- src/app/(withAdminLayout)/admin/login/page.tsx
+++ src/app/(withAdminLayout)/admin/login/page.tsx
@@ -5,7 +5,7 @@ import React, { FC } from "react";
 const AdminLogin: FC = () => {
   return (
     <Row justify={"center"} align={"middle"} style={{ height: "100vh" }}>
-      <Col className="bg-white p-6 rounded shadow" span={4}>
+      <Col className="bg-white p-6 rounded shadow" span={4} md={5} sm={7}>
         <h2 className="text-primary mb-5 text-center ">Metro G Admin Login</h2>
         <LoginPage type="admin" />
       </Col>
