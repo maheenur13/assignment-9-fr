267ff48cd702844f8ce05cd2b3e6ff9a7342450b
maheenur13 <differentmahee13@gmail.com>
2023-11-01 05:10:38 +0600

    loader added

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -4,8 +4,24 @@ import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import AntdRegistry from "./AntdRegistry";
 import { SessionProvider } from "next-auth/react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  const searchParams = useSearchParams();
+  useEffect(() => {
+    // You can now use the current URL
+    NProgress.start();
+  }, [pathname]);
+
+  useEffect(() => {
+    NProgress.done();
+    // You can now use the current URL
+  }, [searchParams]);
   return (
     <Provider store={store}>
       <AntdRegistry>
